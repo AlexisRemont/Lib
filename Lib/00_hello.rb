50c7d34adc6a3bcc07faae2d125bc0f3b596c814
Alexis REMONT <55763711+AlexisRemont@users.noreply.github.com>
2019-10-03 18:45:05 +0200

    Add files via upload

---
+++ Lib/00_hello.rb
@@ -0,0 +1,30 @@
+
+#DEF SAY_HELLO
+
+def say__hello
+    puts "hello" 
+end
+
+puts say__hello
+
+
+
+#DEF SAY_HELLO_FIRST_NAME
+
+def say_hello_first_name (first_name)
+    
+    return "Bonjour #{first_name}"
+end
+
+puts say_hello_first_name
+
+
+
+#DEF COMBO
+def combo
+    puts "Quel est ton prénom?"
+    first_name = gets.chomp
+    "Bonjour#{first_name}"
+
+end
+puts combo
